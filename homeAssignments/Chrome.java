9bb690f743d77a7a4d1f968ee1dc298df54efa5e
Aishu0305 <aishuadhi97@gmail.com>
2025-11-09 16:55:43 +0530

    Add files via upload

---
+++ homeAssignments/Chrome.java
@@ -0,0 +1,19 @@
+package week1.homeAssignments;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     float version = 91.0f;
+     String developer = "Google";
+     boolean isBeta = false;
+     int releaseYear = 2008;
+     char shortCutKey = 'C';
+     System.out.println("Version of Chrome is "+ version);
+     System.out.println("developer is "+developer);
+     System.out.println("isBeta is "+ isBeta);
+     System.out.println("Release Year of Chrome is "+releaseYear);
+     System.out.println("shortCutKey is "+shortCutKey);
+	}
+
+}
